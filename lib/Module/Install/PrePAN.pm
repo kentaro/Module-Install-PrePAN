d705bfc809aed82996877fc3fd84c15203b626e3
Kentaro Kuribayashi <kentarok@gmail.com>
2011-11-09 19:01:37 +0900

    Hacked up M::I::PrePAN

--- lib/Module/Install/PrePAN.pm
+++ lib/Module/Install/PrePAN.pm
@@ -1,33 +1,54 @@
 package Module::Install::PrePAN;
+use 5.008001;
 use strict;
 use warnings;
-use 5.008008;
+use Carp ();
+
 our $VERSION = '0.01';
 
+use base qw(Module::Install::Base);
 
+sub prepan_url {
+    my ($self, $url) = @_;
+    Carp::croak 'missing parameter: $url' if !defined $url;
+    $self->resources(PrePAN => $url);
+}
 
-1;
+!!1;
+
 __END__
 
 =encoding utf8
 
 =head1 NAME
 
-Module::Install::PrePAN -
+Module::Install::PrePAN - Designate a URL of PrePAN page of a module
 
 =head1 SYNOPSIS
 
-  use Module::Install::PrePAN;
+  # Makefile.PL
+  prepan_url  'http://prepan.org/module/3Yz7PYrBJG';
 
 =head1 DESCRIPTION
 
-Module::Install::PrePAN is
+Module::Install::PrePAN is a Module::Install extension to designate a
+URL of PrePan page where you requested reviews for your module.
 
 =head1 AUTHOR
 
 Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+=over 4
+
+=item * L<Module::Install>
+
+=item * PrePAN
+
+L<http://prepan.org/>
+
+=back
 
 =head1 LICENSE
 
---
+++ t/01_basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+subtest 'pass' => sub {
+    pass;
+};
+
+done_testing;
