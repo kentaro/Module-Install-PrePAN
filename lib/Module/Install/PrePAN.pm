cdf8daaed4f5d309b4c66b91b01c04adccde488a
Kentaro Kuribayashi <kentarok@gmail.com>
2011-11-09 17:50:44 +0900

    initial import

---
+++ lib/Module/Install/PrePAN.pm
@@ -0,0 +1,39 @@
+package Module::Install::PrePAN;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Module::Install::PrePAN -
+
+=head1 SYNOPSIS
+
+  use Module::Install::PrePAN;
+
+=head1 DESCRIPTION
+
+Module::Install::PrePAN is
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kentaro Kuribayashi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
